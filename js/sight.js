1e9960f08181605ebb187e09fe84c54378ddbd41
ASPB <alindabyamukama@gmail.com>
2021-12-02 00:26:36 +0300

    Completed module13: corrections

--- js/sight.js
+++ js/sight.js
@@ -25,11 +25,13 @@ class SVGElement {
 
   class Sight {
       constructor(selector, width, height) {
-          this.svg = new SVGElement('svg').attr({viewbox: `0 0 ${width} ${height}`}).append(selector);
+          this.svg = new SVGElement('svg').attr({
+              viewbox: `0 0 ${width} ${height}`
+            }).append(selector);
       }
 
       draw(type, attrs) {
-          return SVGElement(type).attr(attrs).append(svg);
+          return new SVGElement(type).attr(attrs).append(this.svg);
       }
   }
 
